adbd554b4d71ebbd34c1db87c4e90619783acf14
watcharakan <job.2538@gmail.com>
2017-10-19 20:22:38 +0700

    IOT
    google

--- src/app.js
+++ src/app.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
+import firebase from 'firebase';
 import {Header} from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+	componentWillMount() {
+	firebase.initializeApp({
+    apiKey: 'AIzaSyBD0CWFUosNDYY94JGmtANpWviOE6bswmQ',
+    authDomain: 'authen-b7a29.firebaseapp.com',
+    databaseURL: 'https://authen-b7a29.firebaseio.com',
+    projectId: 'authen-b7a29',
+    storageBucket: 'authen-b7a29.appspot.com',
+    messagingSenderId: '541051663658'
+  });
+	}
    render() {
     return(
         <View>
         <Header headerText = "Auth" />
-           <Text>An App</Text>
+         <LoginForm/>
         </View>
 
 
---
+++ src/components/LoginForm.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import {TextInput} from 'react-native';
+import {Button, Card,CardSection,Input} from './common';
+
+class LoginForm extends Component{
+	state = {text: ''};
+render()
+   {
+   	return (
+   	 <Card>
+   		<CardSection>
+          <TextInput 
+          value = {this.state.text}
+          onChangeText ={text => this.setState({text})}
+          />
+          
+   		</CardSection>
+
+
+   		<CardSection/>
+
+   		<CardSection>
+          <Button>
+             Login  
+          </Button>
+
+
+   		</CardSection>
+      </Card>
+       
+
+   		);
+
+
+
+   }
+}
+export default LoginForm;
