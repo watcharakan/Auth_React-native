b1fc463cec711509cb635e750e9b55157006e7cf
watcharakan <job.2538@gmail.com>
2017-10-21 22:30:06 +0700

    password
    password

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,29 +1,42 @@
 import React, {Component} from 'react';
+import firebase from 'firebase';
 import {TextInput} from 'react-native';
 import {Button, Card,CardSection,Input} from './common';
 
 class LoginForm extends Component{
-	state = {text: ''};
+	state = {email: '',password:''};
+onButtonPress(){
+	const { email,password } = this.state;
+	firebase.auth().signInWithEmailAndPassword();
+
+	}
 render()
    {
    	return (
    	 <Card>
    		<CardSection>
           <Input 
+          placeholder="user@gmail.com"
           label = "Email"
-          value = {this.state.text}
-          onChangeText ={text => this.setState({text})}
+          value = {this.state.email}
+          onChangeText ={email => this.setState({email})}
           />
           
    		</CardSection>
 
 
    		<CardSection>
-
-   		</CardSection>
-
+         <Input 
+          secureTextEntry
+          placeholder="password"
+          label = "Password"
+          value = {this.state.password}
+          onChangeText ={password => this.setState({password})}
+          />
+   		
+         </CardSection>
    		<CardSection>
-          <Button>
+          <Button onPress ={this.onButtonPress.bind(this)}>
              Login  
           </Button>
 
